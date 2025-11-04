e0ad0f0b416c262e3ebe607e55254358a018a311
oilater <oilater@naver.com>
2025-11-04 22:38:06 +0900

    refactor: DatePicker onChange 리펙토링

--- src/components/Search/DatePicker/DatePicker.tsx
+++ src/components/Search/DatePicker/DatePicker.tsx
@@ -7,10 +7,6 @@ import * as styles from './DatePicker.css';
 export function DatePicker() {
   const { dateRange, updateDateRange } = useContext(SearchContext);
   
-  const onChangeDate = (start: string, end: string) => {
-    updateDateRange(start, end);
-  };
-
   return (
     <div className={styles.datePickerContainer}>
       <Label>기간</Label>
@@ -18,14 +14,14 @@ export function DatePicker() {
         <Input
           type="date"
           value={dateRange.startDate}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChangeDate(e.target.value, dateRange.endDate)}
+          onChange={(e) => updateDateRange(e.target.value, dateRange.endDate)}
           className={styles.dateInput}
         />
         <span className={styles.separator}>~</span>
         <Input
           type="date"
           value={dateRange.endDate}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChangeDate(dateRange.startDate, e.target.value)}
+          onChange={(e) => updateDateRange(dateRange.startDate, e.target.value)}
           className={styles.dateInput}
         />
       </div>
