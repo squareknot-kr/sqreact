db7853673f3a8bc164449022cacc622db15cec76
oilater <oilater@naver.com>
2025-11-12 17:55:18 +0900

    fix: 빌드 이슈 해결

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,10 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const isBuild = process.env.STORYBOOK_BUILD === 'true' || process.env.NODE_ENV === 'production';
 const basePath = isBuild ? '/sqreact/' : '/';
@@ -36,6 +41,7 @@ const config: StorybookConfig = {
       resolve: {
         alias: {
           '@storybook/addon-docs/mdx-react-shim': '@storybook/addon-docs/dist/mdx-react-shim.js',
+          '@': resolve(__dirname, '../src'),
         },
       },
     });
